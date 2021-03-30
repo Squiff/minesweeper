e03c15dfc97c5660621998133a7a4966a70cca61
Paul Asquith <p.n.asquith@gmail.com>
2021-03-30 19:02:26 +0100

    added image bundling

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,20 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template:'./src/index.html',
-      filename: 'index.html'
-    })
-  ]
+    entry: './src/main.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,34 +1,36 @@
-const path = require("path");
+const path = require('path');
 const common = require('./webpack.common.js');
-const {merge} = require("webpack-merge");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-
 
 module.exports = merge(common, {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'img/[name].[contenthash:8][ext]',
     },
-    plugins: [
-        new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
-        new CleanWebpackPlugin()
-    ],
+    plugins: [new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }), new CleanWebpackPlugin()],
     module: {
-        rules:[
+        rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            }
-        ]
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: '' },
+                    },
+                    'css-loader',
+                ],
+            },
+        ],
     },
     optimization: {
         minimizer: [
-            
-            '...',                      //  `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`)
-            new CssMinimizerPlugin()    // minimize CSS
-          ]
-      }
-})
+            '...', //  `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`)
+            new CssMinimizerPlugin(), // minimize CSS
+        ],
+    },
+});
