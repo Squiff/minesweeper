e7e68dbb2cf54a64a6118b471b66cc1d52ada90d
Paul Asquith <p.n.asquith@gmail.com>
2021-03-31 13:45:38 +0100

    remove font-awsome script

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,38 +1,38 @@
-import { Minesweeper } from "./modules/minesweeper.js";
-import { NewGameForm } from "./modules/newgameform.js";
-import { gameOptions } from "./modules/gameoptions.js";
-import { Scores } from "./modules/scores.js"
-import { ScoreSelect } from "./modules/scoreselect.js";
-
+import { Minesweeper } from './modules/minesweeper.js';
+import { NewGameForm } from './modules/newgameform.js';
+import { gameOptions } from './modules/gameoptions.js';
+import { Scores } from './modules/scores.js';
+import { ScoreSelect } from './modules/scoreselect.js';
+import { watchIcons } from './modules/iconManager';
 
 export class App {
-    constructor(){
+    constructor() {
         this.components = {
             scoreSelect: new ScoreSelect(),
             scores: new Scores(),
             minesweeper: new Minesweeper(gameOptions.easy),
-            newGameForm: new NewGameForm()
+            newGameForm: new NewGameForm(),
         };
 
         this.startUp();
     }
 
-    startUp(){
+    startUp() {
+        watchIcons();
         this.bindAppEvents();
         this.components.scoreSelect.selectDifficulty('easy');
     }
 
-    bindAppEvents(){
-        const {scoreSelect, scores, minesweeper, newGameForm}  = this.components;
+    bindAppEvents() {
+        const { scoreSelect, scores, minesweeper, newGameForm } = this.components;
 
         newGameForm.events.newGame.addEventListener((options) => minesweeper.newGame(options));
-        minesweeper.events.gameWon.addEventListener((args) => this.addHighScore(args.difficulty, args.time));
-        scoreSelect.events.optionClicked.addEventListener((difficulty) => scores.renderTable(difficulty));        
-    }  
-    
-    addHighScore(difficulty, time){
+        minesweeper.events.gameWon.addEventListener(({ difficulty, time }) => this.addHighScore(difficulty, time));
+        scoreSelect.events.optionClicked.addEventListener((difficulty) => scores.renderTable(difficulty));
+    }
+
+    addHighScore(difficulty, time) {
         this.components.scores.addHighScore(difficulty, time);
         this.components.scoreSelect.selectDifficulty(difficulty);
     }
-
 }
