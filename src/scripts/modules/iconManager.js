f4ff7fdfa900a80cc8cd6fa439fed3308f27fb90
Paul Asquith <p.n.asquith@gmail.com>
2021-04-03 22:25:50 +0100

    add footer style tweaks

--- src/scripts/modules/iconManager.js
+++ src/scripts/modules/iconManager.js
@@ -3,9 +3,10 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faBomb, faFlag, faMedal, faMousePointer } from '@fortawesome/free-solid-svg-icons';
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 // only package up these icons
-library.add(faQuestionCircle, faBomb, faFlag, faMedal, faMousePointer);
+library.add(faQuestionCircle, faBomb, faFlag, faMedal, faMousePointer, faGithub);
 
 // Replace any existing <i> tags with <svg> and set up a MutationObserver to
 // continue doing this as the DOM changes.
